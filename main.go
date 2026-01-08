8d5c6274a7d494580a59314f09904192cced6110
Shoga Ayodeji <ayodejishoga1@gmail.com>
2026-01-08 15:47:17 +0100

    Two Pointers

--- main.go
+++ main.go
@@ -1,42 +1,94 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"unicode"
 )
 
 func main() {
+	slice1 := []int{1, 3, 3, 5, 2, 2, 6, 7, 8, 8}
+	removeDuplicates(slice1)
 
-	var reader = bufio.NewReader(os.Stdin)
+	//PRINT RESULT
+	fmt.Println(slice1)
+}
 
-	fmt.Print("Enter your name: ")
-	var name, nameError = reader.ReadString('\n')
-	if nameError != nil {
-		fmt.Println("Error: ", nameError.Error())
-		return
-	}
-	if strings.TrimSpace(name) == "ayodeji" || strings.TrimSpace(name) == "Ayodeji" {
-		fmt.Println("Your name can't be Ayodeji because the owner of this app is Ayodeji")
-		return
+// REMOVE DUPLICATES IN A SLICE
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
 	}
 
-	fmt.Print("Enter your age: ")
+	slow := 0
 
-	var age, ageErr = reader.ReadString('\n')
-	if ageErr != nil {
-		fmt.Println("Error: ", ageErr.Error())
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
 	}
-	ageNum, ageNumErr := strconv.Atoi(strings.TrimSpace(age))
-	if ageNumErr != nil {
-		fmt.Println("Error: ", ageNumErr.Error())
-		return
+
+	return slow + 1
+}
+
+func CheckPalindrome(word string) bool {
+	//CONVERT TO LOWER CASE AND RUNE
+	var chars []rune
+	for _, char := range word {
+		if unicode.IsLetter(char) || unicode.IsDigit(char) {
+			chars = append(chars, unicode.ToLower(char))
+		}
 	}
-	if ageNum < 18 {
-		fmt.Println("Your age is too low")
-		return
+
+	left := 0
+	right := len(chars) - 1
+
+	for left < right {
+		if chars[left] != chars[right] {
+			return false
+		} else {
+			left++
+			right--
+		}
+		return true
 	}
-	fmt.Println("Welcome ", name, "you said you are", ageNum, "years old.")
+	return false
+
+}
+
+func TwoSumBruteForce(nums []int, target int) []int {
+	n := len(nums)
+
+	// Outer loop: pick first number
+	for i := 0; i < n; i++ {
+		// Inner loop: pick second number
+		for j := i + 1; j < n; j++ {
+			// Check if they sum to target
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{-1, -1}
+}
+
+// TwoSum : FIND THE SUM OF TWO ELEMENTS IN THE SLICE THAT EQUALS THE TARGET
+func TwoSum(numbers []int, target int) []int {
+	var left = 0
+	var right = len(numbers) - 1
+
+	for left < right {
+		sum := numbers[left] + numbers[right]
+		if sum == target {
+			return []int{left, right}
+		} else if sum < target {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	//SUM NOT FOUND
+	return []int{-1, -1}
 }
