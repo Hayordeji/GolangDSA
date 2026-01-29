9de4a5e836f11318b5ba606d9cbb01fa49a6a781
Shoga Ayodeji <ayodejishoga1@gmail.com>
2026-01-29 20:43:23 +0100

    DP_Problems

--- main.go
+++ main.go
@@ -3,3 +3,19 @@ package main
 func main() {
 
 }
+
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	prev2, prev1 := 1, 2
+
+	for i := 3; i <= n; i++ {
+		current := prev1 + prev2
+		prev2 = prev1
+		prev1 = current
+	}
+
+	return prev1
+}
